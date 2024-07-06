bf37aa6a85d4350ecf38b361740a740e3a388c01
taturl <taturl@github.com>
2024-07-06 15:43:40 +0300

    Update footer

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -95,46 +95,19 @@ const config: Config = {
       style: 'dark',
       links: [
         {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Tutorial',
-              to: '/docs/intro',
-            },
-          ],
+          label: 'Etusivulle',
+          to: '/',
         },
         {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
-            },
-          ],
+          label: 'Kirjoihin',
+          to: '/docs/7-9/aidinkieli',
         },
         {
-          title: 'More',
-          items: [
-            {
-              label: 'Blog',
-              to: '/blog',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
-            },
-          ],
+          label: 'Osallistu',
+          href: 'https://github.com/facebook/docusaurus',
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Sisältö on käytettävissä lisenssillä MIT, ellei toisin ole mainittu.`,
     },
     prism: {
       theme: prismThemes.github,
