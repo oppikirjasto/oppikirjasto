3fd8f8b7b527d077e2974f9ab6aabad9da0624bc
taturl <taturl@github.com>
2024-07-07 19:02:41 +0300

    Translate ui components

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -42,7 +42,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/oppikirjasto/oppikirjasto',
+            'https://github.com/oppikirjasto/oppikirjasto/blob/main',
           remarkPlugins: [remarkMath],
           rehypePlugins: [rehypeKatex],
         },
