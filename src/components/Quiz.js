ba7a87c1577e69269d67bce7ed3b33c149697bef
taturl <taturl@github.com>
2024-07-08 17:49:16 +0300

    Fix console errors on unique keys

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -6,12 +6,12 @@ export default function Quiz({subject, chapter}) {
 	const questions = allQuestions[subject][chapter]
 
 	const [currentQuestion, setCurrentQuestion] = useState(0);
-	const [selectedAnswerWhy, setSelectedAnswerWhy] = useState('');
+	const [selectedAnswer, setSelectedAnswer] = useState(0);
 	const [showResult, setShowResult] = useState(false);
 	const [quizHasEnded, setQuizHasEnded] = useState(false);
 
-	const handleAnswerOptionClick = (answerWhy) => {
-		setSelectedAnswerWhy(answerWhy)
+	const handleAnswerOptionClick = (i) => {
+		setSelectedAnswer(i)
 		setShowResult(true)
 	};
 
@@ -39,13 +39,13 @@ export default function Quiz({subject, chapter}) {
 					<>
 						{showResult ? (
 							<div className='result-section'>
-								<div className='result-text'>{selectedAnswerWhy}</div>
-								<button class="next-button" onClick={() => handleNextClick()}>Seuraavaan</button>
+								<div className='result-text'>{questions[currentQuestion].answerOptions[selectedAnswer].answerWhy}</div>
+								<button className="next-button" onClick={() => handleNextClick()}>Seuraavaan</button>
 							</div>
 						) : (
 							<div className='answer-section'>
-								{questions[currentQuestion].answerOptions.map((answerOption) => (
-									<button class="answer-button" onClick={() => handleAnswerOptionClick(answerOption.answerWhy)}>{answerOption.answerText}</button>
+								{questions[currentQuestion].answerOptions.map((answerOption, i) => (
+									<button className="answer-button" key={i} onClick={() => handleAnswerOptionClick(i)}>{answerOption.answerText}</button>
 								))}
 							</div>
 						)}
