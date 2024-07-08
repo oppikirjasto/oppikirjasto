f55daa83d8ed59be4166d09d57e1ddcd03717ef4
taturl <taturl@github.com>
2024-07-08 13:21:41 +0300

    Add media player

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -2,8 +2,14 @@
 // without import statements
 import MDXComponents from '@theme-original/MDXComponents';
 import Quiz from '@site/src/components/Quiz';
+import ReactPlayer from 'react-player';
+
+ReactPlayer.defaultProps.width = '100%'
+ReactPlayer.defaultProps.height = '100%'
+ReactPlayer.defaultProps.controls = true
 
 export default {
   ...MDXComponents,
   Quiz,
+  ReactPlayer
 };
