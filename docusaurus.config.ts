b26074c5880130fd932d56fd4e9d3bde1503d1b7
taturl <taturl@github.com>
2024-07-11 12:13:06 +0300

    Add content for 1-6

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -91,9 +91,57 @@ const config: Config = {
       items: [
         {
           type: 'docSidebar',
-          sidebarId: 'sevenToNineSidebar',
+          sidebarId: 'sidebar1',
           position: 'left',
-          label: 'Yläkoulu',
+          label: '1',
+        },
+        {
+          type: 'docSidebar',
+          sidebarId: 'sidebar2',
+          position: 'left',
+          label: '2',
+        },
+        {
+          type: 'docSidebar',
+          sidebarId: 'sidebar3',
+          position: 'left',
+          label: '3',
+        },
+        {
+          type: 'docSidebar',
+          sidebarId: 'sidebar4',
+          position: 'left',
+          label: '4',
+        },
+        {
+          type: 'docSidebar',
+          sidebarId: 'sidebar5',
+          position: 'left',
+          label: '5',
+        },
+        {
+          type: 'docSidebar',
+          sidebarId: 'sidebar6',
+          position: 'left',
+          label: '6',
+        },
+        {
+          type: 'docSidebar',
+          sidebarId: 'sidebar7',
+          position: 'left',
+          label: '7',
+        },
+        {
+          type: 'docSidebar',
+          sidebarId: 'sidebar8',
+          position: 'left',
+          label: '8',
+        },
+        {
+          type: 'docSidebar',
+          sidebarId: 'sidebar9',
+          position: 'left',
+          label: '9',
         },
         {
           href: 'https://github.com/oppikirjasto/oppikirjasto',
@@ -111,7 +159,7 @@ const config: Config = {
         },
         {
           label: 'Kirjoihin',
-          to: '/docs/7-9/aidinkieli',
+          to: '/docs/9/aidinkieli',
         },
         {
           label: 'Osallistu',
